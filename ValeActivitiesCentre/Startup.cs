57672d681543008108104aa8631e4d91e5bc834c
William Foster <William_1foster@yahoo.co.uk>
2019-04-28 14:06:03 +0100

    Add project files.

---
+++ ValeActivitiesCentre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ValeActivitiesCentre.Startup))]
+namespace ValeActivitiesCentre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
