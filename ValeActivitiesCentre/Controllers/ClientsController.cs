d2041546a65b5f3b5f2dd1b602e2fd8194b9d4d8
William Foster <William_1foster@yahoo.co.uk>
2019-05-09 11:27:23 +0100

    Clients Controller and Staffs Controller is now constructed. All controllers are being made as MVC 5 controller with views, using EF

---
+++ ValeActivitiesCentre/Controllers/ClientsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ValeActivitiesCentre.DAL;
+using ValeActivitiesCentre.Models;
+
+namespace ValeActivitiesCentre.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private ValeDbContext db = new ValeDbContext();
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            var clients = db.Clients.Include(c => c.ClientProfile).Include(c => c.Person).Include(c => c.RiskAssessment);
+            return View(clients.ToList());
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientID = new SelectList(db.ClientProfiles, "ClientProfileID", "BestComunicationApproach");
+            ViewBag.ClientID = new SelectList(db.People, "PersonID", "FirstName");
+            ViewBag.ClientID = new SelectList(db.RiskAssessments, "RiskAssessmentID", "PhysicalHealthNotes");
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClientID,Funding")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clients.Add(client);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientID = new SelectList(db.ClientProfiles, "ClientProfileID", "BestComunicationApproach", client.ClientID);
+            ViewBag.ClientID = new SelectList(db.People, "PersonID", "FirstName", client.ClientID);
+            ViewBag.ClientID = new SelectList(db.RiskAssessments, "RiskAssessmentID", "PhysicalHealthNotes", client.ClientID);
+            return View(client);
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientID = new SelectList(db.ClientProfiles, "ClientProfileID", "BestComunicationApproach", client.ClientID);
+            ViewBag.ClientID = new SelectList(db.People, "PersonID", "FirstName", client.ClientID);
+            ViewBag.ClientID = new SelectList(db.RiskAssessments, "RiskAssessmentID", "PhysicalHealthNotes", client.ClientID);
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClientID,Funding")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientID = new SelectList(db.ClientProfiles, "ClientProfileID", "BestComunicationApproach", client.ClientID);
+            ViewBag.ClientID = new SelectList(db.People, "PersonID", "FirstName", client.ClientID);
+            ViewBag.ClientID = new SelectList(db.RiskAssessments, "RiskAssessmentID", "PhysicalHealthNotes", client.ClientID);
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client client = db.Clients.Find(id);
+            db.Clients.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ValeActivitiesCentre/Controllers/StaffsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ValeActivitiesCentre.DAL;
+using ValeActivitiesCentre.Models;
+
+namespace ValeActivitiesCentre.Controllers
+{
+    public class StaffsController : Controller
+    {
+        private ValeDbContext db = new ValeDbContext();
+
+        // GET: Staffs
+        public ActionResult Index()
+        {
+            var staffs = db.Staffs.Include(s => s.Person);
+            return View(staffs.ToList());
+        }
+
+        // GET: Staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.StaffID = new SelectList(db.People, "PersonID", "FirstName");
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StaffID,Department,JobPosition,Profile")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Staffs.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StaffID = new SelectList(db.People, "PersonID", "FirstName", staff.StaffID);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StaffID = new SelectList(db.People, "PersonID", "FirstName", staff.StaffID);
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StaffID,Department,JobPosition,Profile")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StaffID = new SelectList(db.People, "PersonID", "FirstName", staff.StaffID);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            db.Staffs.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
